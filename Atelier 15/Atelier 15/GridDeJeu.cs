d05648059bc097e476669cc469426916df26e3ce
Benoit Charbonneau <becharbonneau@videotron.ca>
2017-02-07 15:06:18 -0500

    gossage

--- Atelier 15/Atelier 15/GridDeJeu.cs
+++ Atelier 15/Atelier 15/GridDeJeu.cs
@@ -12,31 +12,32 @@ using Microsoft.Xna.Framework.Media;
 
 namespace AtelierXNA
 {
-    /// <summary>
-    /// This is a game component that implements IUpdateable.
-    /// </summary>s
-    public class GridDeJeu : Microsoft.Xna.Framework.GameComponent
-    {
-        Vector3 Étendue { get; set; }
-        public Vector2 Charpente { get; private set; }
-        public Vector2 Delta { get; private set; }
-        public bool[,] TableauGrid { get; set; }
+    /// <summary> 
+    /// This is a game component that implements IUpdateable. 
+     /// </summary> 
+     public class GridDeJeu : Microsoft.Xna.Framework.GameComponent 
+     { 
+         Vector3 Étendue { get; set; } 
+         public Vector2 Charpente { get; private set; } 
+         public Vector2 Delta { get; private set; } 
+         public bool[,] TableauGrid { get; set; } 
+ 
 
-        //recoit en parametre l'étendu et la charpente (grandeur et dimension du grid)
-        public GridDeJeu(Game game, Vector3 étendue, Vector2 charpente)
-            : base(game)
-        {
-            Étendue = étendue;
-            Charpente = charpente;
-            Delta = new Vector2(Étendue.X / Charpente.X, Étendue.Z / Charpente.Y);
-            TableauGrid = new bool[(int)Charpente.X, (int)Charpente.Y];
-            for (int i = 0; i < Charpente.X; i++)
-            {
-                for (int j = 0; j < Charpente.Y; j++)
-                {
-                    TableauGrid[i, j] = true;
-                }
-            }
-        }
-    }
- }
+         //recoit en parametre l'étendu et la charpente (grandeur et dimension du grid) 
+         public GridDeJeu(Game game, Vector3 étendue, Vector2 charpente) 
+             : base(game) 
+         { 
+             Étendue = étendue; 
+             Charpente = charpente; 
+             TableauGrid = new bool[(int)Charpente.X, (int)Charpente.Y]; 
+             for (int i = 0; i < Charpente.X; i++) 
+             { 
+                 for (int j = 0; j < Charpente.Y; j++) 
+                 { 
+                     TableauGrid[i, j] = true; 
+                 } 
+             } 
+             Delta = new Vector2(Étendue.X / Charpente.X, Étendue.Z / Charpente.Y); 
+         } 
+     } 
+}
