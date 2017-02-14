aa53867cb4bd4e4c1631bac259694f84e7045b62
David <dav_rl@hotmail.com>
2017-02-13 21:18:37 -0500

    Control de phase

--- Atelier 15/Atelier 15/Atelier.cs
+++ Atelier 15/Atelier 15/Atelier.cs
@@ -61,6 +61,9 @@ namespace AtelierXNA
             Components.Add(new Afficheur3D(this));
             Components.Add(new Terrain(this, 1f, Vector3.Zero, Vector3.Zero, new Vector3(256, 25, 256), new Vector2(64, 64), INTERVALLE_MAJ_STANDARD));
             GenerateurProcedural generateurProc = new GenerateurProcedural(this, Vector3.Zero, new Vector3(256, 25, 256), new Vector2(64, 64));
+            ControlePhaseDeJeu controlePhase = new ControlePhaseDeJeu(this, 180f, 120f);
+            Components.Add(controlePhase);
+            Services.AddService(typeof(ControlePhaseDeJeu), controlePhase);
             CaméraJeu = new Caméra3rdPerson(this, positionCaméra, cibleCaméra, Vector3.Up, INTERVALLE_MAJ_STANDARD);
             Services.AddService(typeof(Caméra), CaméraJeu);
             Components.Add(generateurProc);
